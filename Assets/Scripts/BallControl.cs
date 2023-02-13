fcbdb592755b13ffa83c97b11e291fc187791f27
Ellen Lo <lowing@bu.edu>
2023-02-12 23:44:02 -0800

    Katamari pick up objects!

--- Assets/Scripts/BallControl.cs
+++ Assets/Scripts/BallControl.cs
@@ -7,6 +7,7 @@ public class BallControl : MonoBehaviour
 
     // Character components
     private Rigidbody rb;
+    private SphereCollider cl;
 
     // Player (da prince) state
     public GameObject player;
@@ -18,10 +19,16 @@ public class BallControl : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        cl = GetComponent<SphereCollider>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
+    {
+        Roll();
+    }
+
+    void Roll()
     {
         Vector3 inFront = player.transform.position + player.transform.forward;
         transform.position = new Vector3(inFront.x, 0, inFront.z);
@@ -30,6 +37,15 @@ public class BallControl : MonoBehaviour
         Quaternion forwardRotation = Quaternion.AngleAxis(playerControl.currentVelocity.z * Time.fixedDeltaTime * turn, player.transform.right);
         Quaternion rightRotation = Quaternion.AngleAxis(-playerControl.currentVelocity.x * Time.fixedDeltaTime * turn, player.transform.forward);
         transform.rotation = forwardRotation * rightRotation * playerUpRotation * transform.rotation;
-
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.transform.parent.name == "objectsToGather")
+        {
+            GameObject clone = Instantiate(other.gameObject, other.transform.position, other.transform.rotation, transform);
+            clone.transform.localScale = new Vector3(clone.transform.localScale.x / transform.localScale.x, clone.transform.localScale.y / transform.localScale.y, clone.transform.localScale.z / transform.localScale.z);
+            Destroy(other.gameObject);
+        }
     }
 }
