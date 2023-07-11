49b948348565cc24d06ff808398a0a5f85918a32
Ellen Lo <lowing@bu.edu>
2023-07-11 13:00:26 -0700

    Poisson disc sampling with uniform radius

---
+++ Assets/Scripts/desktop-rb/BetterObjectsSpawner.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BetterObjectsSpawner : MonoBehaviour
+{
+    [SerializeField] private Vector2 regionSize = Vector2.one;
+    [SerializeField] private float radius = 1;
+    [SerializeField] private int rejectionSamples = 30;
+    [SerializeField] private Vector2 regionOffset = Vector2.zero;
+
+    private List<Vector2> points;
+
+    void Start()
+    {
+        points = PoissonDiscSampling.GeneratePoints2D(radius, regionSize, rejectionSamples);
+        foreach(Vector2 point in points) {
+            int idx = (int) Random.Range(0, transform.childCount);
+            GameObject obj = transform.GetChild(idx).gameObject;
+            GameObject spawned = Instantiate(obj, new Vector3(point.x + regionOffset.x, obj.transform.position.y, point.y + regionOffset.y), obj.transform.rotation, transform);
+            float randomScale = Random.Range(0.3f, 1.8f);
+            
+        }
+    }
+}
